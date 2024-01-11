9000a881d360412c370483264808619208af0e2e
Mariana Vorotniak <mvorodeveloper@gmail.com>
2024-01-12 00:04:19 +0200

    Homework 24 - Dirty checking

--- orm-poc/src/main/java/com/orm/poc/dao/Dao.java
+++ orm-poc/src/main/java/com/orm/poc/dao/Dao.java
@@ -1,9 +1,14 @@
 package com.orm.poc.dao;
 
+import com.orm.poc.domain.Pair;
+
+import java.util.List;
 import java.util.Optional;
 
 public interface Dao {
 
   <T, I> Optional<T> findById(Class<T> type, I id);
+
+  <T, I> void update(Class<T> type, I id, List<Pair<String, Object>> fieldNameToValuePairs);
   
 }
--- orm-poc/src/main/java/com/orm/poc/dao/EntityDao.java
+++ orm-poc/src/main/java/com/orm/poc/dao/EntityDao.java
@@ -1,5 +1,6 @@
 package com.orm.poc.dao;
 
+import com.orm.poc.domain.Pair;
 import com.orm.poc.domain.exception.OrmPocException;
 import com.orm.poc.mapper.EntityMapper;
 import com.orm.poc.utils.ReflectionUtils;
@@ -8,11 +9,15 @@ import javax.sql.DataSource;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.Collections;
+import java.util.List;
 import java.util.Optional;
+import java.util.stream.Collectors;
+
+import static com.orm.poc.utils.QueryStrings.*;
 
 public class EntityDao implements Dao {
-
-  private static final String FIND_BY_ID = "SELECT * FROM %s WHERE %s = ?";
   
   private final DataSource dataSource;
   
@@ -26,9 +31,9 @@ public class EntityDao implements Dao {
   @Override
   public <T, I> Optional<T> findById(Class<T> type, I id) {
     try (Connection connection = this.dataSource.getConnection();
-      PreparedStatement statement = connection.prepareStatement(this.getFormattedQuery(type))) {
+      PreparedStatement statement = connection.prepareStatement(this.getFindByIdQuery(type))) {
 
-      statement.setObject(1, id);
+      this.populatePreparedStatement(statement, id);
       ResultSet resultSet = statement.executeQuery();
 
       return resultSet.next() ? Optional.of(this.entityMapper.mapToEntity(resultSet, type)) : Optional.empty();
@@ -38,9 +43,46 @@ public class EntityDao implements Dao {
               String.format("Error occurred finding entity [%s] with id [%s]", type.getSimpleName(), id), e);
     }
   }
+
+  @Override
+  public <T, I> void update(Class<T> type, I id, List<Pair<String, Object>> fieldNameToValuePairs) {
+    try (Connection connection = this.dataSource.getConnection();
+         PreparedStatement statement = connection.prepareStatement(this.getUpdateQuery(type, fieldNameToValuePairs))) {
+
+      this.populatePreparedStatement(statement, id, fieldNameToValuePairs);
+      statement.executeUpdate();
+      
+    } catch (Exception e) {
+      throw new OrmPocException(
+              String.format("Error occurred updating entity [%s] with id [%s]", type.getSimpleName(), id), e);
+    }
+  }
+
+  private <T> String getFindByIdQuery(Class<T> type) {
+    return FIND_BY_ID_QUERY.formatted(ReflectionUtils.getTableName(type), ReflectionUtils.getIdName(type));
+  }
   
-  private <T> String getFormattedQuery(Class<T> type) {
-    return FIND_BY_ID.formatted(ReflectionUtils.getTableName(type), ReflectionUtils.getIdName(type));
+  private <T> String getUpdateQuery(Class<T> type, List<Pair<String, Object>> fieldNameToValuePairs) {
+    String setValues = fieldNameToValuePairs
+            .stream()
+            .map(pair -> SET_VALUE.formatted(pair.getLeft()))
+            .collect(Collectors.joining(","));
+    
+    return UPDATE_QUERY.formatted(ReflectionUtils.getTableName(type), setValues, ReflectionUtils.getIdName(type));
+  }
+
+  private void populatePreparedStatement(PreparedStatement statement, Object id) throws SQLException {
+    this.populatePreparedStatement(statement, id, Collections.emptyList());
+  }
+  
+  private void populatePreparedStatement(PreparedStatement statement, 
+                                         Object id, 
+                                         List<Pair<String, Object>> fieldNameToValuePairs) throws SQLException {
+    int i;
+    for (i = 1; i <= fieldNameToValuePairs.size(); i++) {
+      statement.setObject(i, fieldNameToValuePairs.get(i - 1).getRight());
+    }
+    statement.setObject(i, id);
   }
   
 }
